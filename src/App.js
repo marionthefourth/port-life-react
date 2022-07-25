9f60040741b74703643ec5a19335038a1560e02b
Numra Tariq <numra.tariq@altamiracorp.com>
2022-07-25 11:44:05 -0400

    Setting up login page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Viewer } from "resium";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Dashboard } from './Dashboard';
+import Login from './Login';
 
 function App() {
-  return <Viewer full/>
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/viewer" element={<Viewer full />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+
 }
 
 export default App;
